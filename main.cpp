e6fd31fa8784f60caa18448befb900c8ce5fca44
kurocifer <draculemihawk232@gmail.com>
2026-04-20 22:47:13 +0100

    add unstaged files

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "logger.h"
+#include "cmdLineParser.h"
+
+int main(int argc, char **argv)
+{
+    TCmdLineParser cmdParser;
+    std::shared_ptr<TLogger> logger = TLogger::getInstance();
+
+    cmdParser.parseCmdLine(argc, argv);
+
+    if (cmdParser.isLogLevelSet())
+    {
+        switch (cmdParser.getLogLevel())
+        {
+            case 0: logger->setLogLevel(ELogLevel::NONE); break;
+            case 1: logger->setLogLevel(ELogLevel::ERROR); break;
+            case 2: logger->setLogLevel(ELogLevel::WARN); break;
+            case 3: logger->setLogLevel(ELogLevel::INFO); break;
+            case 4: logger->setLogLevel(ELogLevel::DEBUG); break;
+        }
+    }
+
+    if(!cmdParser.isRomFileNameset())
+    {
+        logger->log("No ROM path provided", ELogLevel::ERROR);
+        exit(1);
+    }
+
+    logger->log("ROM Path: " +cmdParser.getRomFileName(), ELogLevel::INFO);
+
+    return 0;
+}
